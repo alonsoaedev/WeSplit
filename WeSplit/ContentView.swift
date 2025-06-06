eec06303ef7bb3f4fe1190d1f93ea52957bc314b
Alonso Acosta <alonso.ae@hotmail.com>
2025-06-06 10:18:51 -0600

    Use conditional modifier in the total amount to show when the tip is added

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -58,6 +58,7 @@ struct ContentView: View {
                         totalAmount,
                         format: .currency(code: Locale.current.currency?.identifier ?? "USD")
                     )
+                    .foregroundStyle(tipPercentage == 0 ? Color.red : Color .blue)
                 }
                 
                 Section("Amount per person") {
